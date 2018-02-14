a28cdbb2d7f592016f2173c3b3a672bfe1b90d50
Oscar Tellez <oscartzgz@gmail.com>
2018-02-14 13:43:00 -0600

    Registro/Ingreso con Facebook habilitado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214184427) do
+ActiveRecord::Schema.define(version: 20180214193753) do
 
   create_table "abouts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20180214184427) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

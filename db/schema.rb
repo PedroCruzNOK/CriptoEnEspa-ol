c4a755c151aa5a1c7d033f8a0dbdda2977788b6f
José Luis Anduaga Islas <Anduaga@localhost.localdomain>
2018-03-01 15:30:16 -0600

    generaron vistas austeras de acerca de y asistencia

--- db/schema.rb
+++ db/schema.rb
@@ -60,16 +60,6 @@ ActiveRecord::Schema.define(version: 20180227192007) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "news", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "url"
-    t.string "source"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

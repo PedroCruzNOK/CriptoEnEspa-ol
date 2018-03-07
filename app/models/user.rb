373a918eb1e1d5456be5a50e078c38be8c9c941a
José Luis Anduaga Islas <Anduaga@localhost.localdomain>
2018-03-07 15:00:06 -0600

    chat sin implemetacion de ActionCable

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,14 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
+      # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       user.skip_confirmation!
     end
   end
-  
+
+  #Esto es de el chat
+    has_many :messages
+    has_many :conversations, foreign_key: :sender_id
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305165423) do
+ActiveRecord::Schema.define(version: 20180307200356) do
 
   create_table "abouts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,11 +53,30 @@ ActiveRecord::Schema.define(version: 20180305165423) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string "level"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "privacy_policies", force: :cascade do |t|

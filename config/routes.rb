457331285f64faf5e867a22bc90ee21c0cd1ec9b
Oscar Tellez <oscartzgz@gmail.com>
2018-03-09 14:35:58 -0600

    Se crea el modelo para las lecciones de cada curso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lessons
   mount ActionCable.server => "/cable"
 
   get 'management/index'
---
+++ db/migrate/20180309203519_create_lessons.rb
@@ -0,0 +1,12 @@
+class CreateLessons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lessons do |t|
+      t.string :topic,
+      t.text, :video_data
+      t.text :content
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
